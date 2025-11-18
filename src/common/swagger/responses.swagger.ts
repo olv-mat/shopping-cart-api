4841a7475be07cea65e78ed5e484df7ea0b39170
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-11-18 08:41:32 -0300

    refactor: detailing the return of the responses

--- src/common/swagger/responses.swagger.ts
+++ src/common/swagger/responses.swagger.ts
@@ -8,19 +8,67 @@ import {
 } from '@nestjs/swagger';
 
 export const SwaggerBadRequest = () =>
-  ApiBadRequestResponse({ description: 'Bad Request' });
+  ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      example: {
+        message: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  });
 
 export const SwaggerUnauthorized = () =>
-  ApiUnauthorizedResponse({ description: 'Unauthorized' });
+  ApiUnauthorizedResponse({
+    description: 'Unauthorized',
+    schema: {
+      example: {
+        message: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  });
 
 export const SwaggerForbidden = () =>
-  ApiForbiddenResponse({ description: 'Forbidden' });
+  ApiForbiddenResponse({
+    description: 'Forbidden',
+    schema: {
+      example: {
+        message: 'Forbidden',
+        statusCode: 403,
+      },
+    },
+  });
 
 export const SwaggerNotFound = () =>
-  ApiNotFoundResponse({ description: 'Not Found' });
+  ApiNotFoundResponse({
+    description: 'Not Found',
+    schema: {
+      example: {
+        message: 'Not Found',
+        statusCode: 404,
+      },
+    },
+  });
 
 export const SwaggerConflict = () =>
-  ApiConflictResponse({ description: 'Conflict' });
+  ApiConflictResponse({
+    description: 'Conflict',
+    schema: {
+      example: {
+        message: 'Conflict',
+        statusCode: 409,
+      },
+    },
+  });
 
 export const SwaggerInternalServerError = () =>
-  ApiInternalServerErrorResponse({ description: 'Internal Server Error' });
+  ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  });
